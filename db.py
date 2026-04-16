7d5c1e494b64fb9855595d1bad955bf551bc3f66
bettoo <joaco_fer@yahoo.com.ar>
2026-04-16 20:23:19 -0300

    refactor: funcion global 'execute_query' para todos los repositorios

--- db.py
+++ db.py
@@ -16,3 +16,31 @@ def get_server_connection():
         user="root",
         password="fiuba"
     )
+
+def execute_query(query, params=None, modifica_db=False, un_solo_valor=False):
+    conn = None
+    cur = None
+    try:
+        conn = get_connection()
+        cur = conn.cursor(dictionary=True)
+
+        cur.execute(query, params)
+
+        if not modifica_db: 
+            if un_solo_valor:
+                return cur.fetchone()
+            return cur.fetchall()
+        
+        # si la query es un insert o update hay que hacer commit() para que los cambios se guarden en la DB 
+        conn.commit()
+        return cur.lastrowid
+    except Exception as e:
+        if conn: 
+            conn.rollback() # si hay un error en un post o put hay que revertir los cambios hecho a la DB para evitar datos corruptos 
+        e.message = f"Error al ejecutar la query: {str(e)}. Query: {query}, Params: {params}"
+        raise e
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
